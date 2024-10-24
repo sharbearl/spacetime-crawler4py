71a69e511cab148629980a117ecdeddcd7d3488c
Michael Overman <moverman@uci.edu>
2024-10-24 16:02:42 -0700

    add basic functionalatiy but it tried opening zoom LOL

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,13 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    if resp.status != 200:
+        print(resp.error)
+        return list()
+    content = BeautifulSoup(resp.raw_response.content, "lxml")
+    atags = content.select('a')
+    ret = [atag['href'] for atag in atags if is_valid(atag['href'])]
+    return ret
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
