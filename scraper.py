98f694fdcf5bcba4bf24f54018cd158d1573d924
rng4@uci.edu <rng4@uci.edu>
2024-10-27 22:26:17 -0700

    Made some edits to fix

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,20 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    #Beautiful Soup: recommended
+    
+    if resp.status != 200:
+        print("Status code:", resp.status,"| Error Message:", resp.error)
+        return list()
+    if len(resp.raw_response.content) <= 500:
+        print("Low information page:", url)
+        return list()
+    content = BeautifulSoup(resp.raw_response.content, "lxml")
+    atags = content.select('a[href]')
+    # I wanna use a list comprehension so bad but I shouldn't
+    return [atag['href'] for atag in atags]
+        
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -25,7 +39,7 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        regMatch = not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -34,6 +48,65 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        if not regMatch:
+            return False
+        validPaths = set(["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu", "today.uci.edu/department/information_computer_sciences"])
+        # bannedQueries = set(["eventDisplay=day&eventDate=2024"])
+        datePattern = r"\b\d{4}-\d{2}\b"
+        matchQuery = re.search(datePattern, parsed.query)
+        matchPath = re.search(datePattern, parsed.path)
+        if matchQuery or matchPath:
+            return False
+        if "filter" in parsed.query or "ical" in parsed.query or "download" in parsed.query or "login" in parsed.query:
+            return False
+        if "share=" in parsed.query:
+            return False
+        if "/uploads" in parsed.path:
+            return False
+        if len(parsed.fragment) > 0:
+            return False
+        if parsed.hostname == "":
+            return False
+        isValidPath = False
+        for path in validPaths:
+            if ("." + path) in parsed.hostname.lower():
+                isValidPath = True
+                break
+            if ("/" + path ) in parsed.hostname.lower():
+                isValidPath = True
+                break
+        if not isValidPath:
+            return False
+        # obeying informatics robots.txt
+        if "informatics.uci.edu" in parsed.hostname.lower():
+            allowedDomains = {
+                "/wp-admin/admin-ajax.php",
+                "/research/labs-centers/",
+                "/research/areas-of-expertise/",
+                "/research/example-research-projects/",
+                "/research/phd-research/",
+                "/research/past-dissertations/",
+                "/research/masters-research/",
+                "/research/undergraduate-research/",
+                "/research/gifts-grants/"
+            }
+            for allowedDomain in allowedDomains:
+                if parsed.path.startswith(allowedDomain):
+                    return True
+            if parsed.path.startswith('/wp-admin') or parsed.path.startswith('/research'):
+                return False
+        # nothing specificed for:
+        # ics.uci.edu
+        # cs.uci.edu
+        # obeying robots.txt for stat.uci.edu
+        if 'stat.uci.edu' in parsed.hostname:
+            if parsed.path.startswith("/web-admin/admin-ajax.php"):
+                return True
+            return not parsed.path.startswith("/wp-admin")
+        if 'today.uci.edu' in parsed.hostname:
+            return parsed.path.startswith("/iseb")
+
+        return False
 
     except TypeError:
         print ("TypeError for ", parsed)
